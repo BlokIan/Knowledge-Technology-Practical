28ad04b1330a19eb1b441e9ba3dade994a602cc5
BlokIan <i.d.blok@student.rug.nl>
2024-11-21 14:42:30 +0100

    Basic ui-page

--- src/KnowledgeApp.py
+++ src/KnowledgeApp.py
@@ -2,7 +2,20 @@ import kivy
 kivy.require("2.3.0")
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
+
+
+class Chat(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
 
 class KnowledgeApp(App):
     def build(self):
-        return Label(text ="Hello World !")
+        return Chat()
+
+
+if __name__ == "__main__":
+    KnowledgeApp().run()
