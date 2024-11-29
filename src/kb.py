928191db716d101f425f951bba83c558593bfc18
Merlijn Brouwer <m.k.brouwer@student.rug.nl>
2024-11-29 15:20:22 +0100

    kb file

---
+++ src/kb.py
@@ -0,0 +1,18 @@
+from ast import main
+from multiprocessing import Value
+from turtle import position
+import json
+import sys
+
+
+class KnowledgeBase:
+    def __init__():
+        pass
+
+    
+    def read_file(filename):
+        with open(filename, "r") as file:
+            data = json.load(file)
+        return data
+        
+
