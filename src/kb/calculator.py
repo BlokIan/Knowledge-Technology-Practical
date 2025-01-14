b6c159e5d6bd912d319fbbf851732ed610e51e62
Hinke Bolt <h.a.bolt@student.rug.nl>
2025-01-14 20:56:47 +0100

    full working kb and inference system

--- src/kb/calculator.py
+++ src/kb/calculator.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import os
+import math
 WOONQUOTE_FILEPATH = os.path.join(os.getcwd(), "src", "kb", "woonquote.txt")
 ANNUITY_FILEPATH = os.path.join(os.getcwd(), "src", "kb", "annuity.txt")
 
 class User:
-    def __init__(self, income, interest, period):
-        self._income = income
+    def __init__(self, income, interest, period, energy_label, woz, monthly_costs, student_debt):
+        self._income = round(income, -3)
         self._interest = interest
         self._period = period
+        self._energy_label = energy_label
+        self._woz = woz
+        self._month_interest = interest / 100 / 12
+        self._costs = monthly_costs
+        self._student_debt = student_debt
+        self._perc_interest_deduction = 0.3748 # 0.3697
         self._expense_table = self._read_expense_table()
         self._annuity_table = self._read_annuity_table()
+        self._bracket= None
+        self._max_mortgage = None
 
     def _read_expense_table(self):
-        df = pd.read_csv(WOONQUOTE_FILEPATH, delimiter="\t", encoding="utf-8")
+        file_name = WOONQUOTE_FILEPATH
+        df = pd.read_csv(file_name, delimiter="\t", encoding="utf-8")
 
         df.columns = df.columns.str.strip()
         df["Income"] = (
@@ -47,16 +57,20 @@ class User:
         return None
     
     def _find_max_expense(self):
-        bracket = self._find_interest_bracket(self._interest, self._expense_table.columns)
-        if bracket is None:
+        self._bracket = self._find_interest_bracket(self._interest, self._expense_table.columns)
+        if self._bracket is None:
             return "Invalid interest rate."
-        if self._income in self._expense_table.index:
-            return float(self._expense_table.loc[self._income, bracket])
+        income = self._income
+        if self._income < 28000:
+            income = 28000
+        if income in self._expense_table.index:
+            return self._expense_table.loc[income, self._bracket]
         else:
             return "Invalid income."
-
+        
     def _read_annuity_table(self):
-        df = pd.read_csv(ANNUITY_FILEPATH, delimiter="\t", encoding="utf-8")
+        file_name = ANNUITY_FILEPATH
+        df = pd.read_csv(file_name, delimiter="\t", encoding="utf-8")
 
         df.columns = df.columns.str.strip()  
         del df["Per maand"] 
@@ -76,31 +90,145 @@ class User:
         return df 
     
     def _find_annuity_factor(self):
-        if self._interest in self._annuity_table.index:
-            return float(self._annuity_table.loc[self._interest, self._period])
+        if math.ceil(self._interest*10)/10 in self._annuity_table.index:
+            return self._annuity_table.loc[math.ceil(self._interest*10)/10, self._period]
         else:
             return "Invalid interest rate."
+    
+    def _extra_mortgage_energy_label(self):
+        """Returns the extra mortgage a user can receive given the energy label."""
+        match self._energy_label:
+            case "A++++ with energy performance guarantee":
+                return 50000
+            case "A++++":
+                return 40000
+            case "A+++":
+                return 30000
+            case "A++, A+":
+                return 20000
+            case "A, B":
+                return 10000
+            case "C, D":
+                return 5000
+            case "E, F, G":
+                return 0 
+            
+    def _less_mortgage_student_debt(self):
+        if type(self._student_debt) == str:
+            return 0
+        factor = 0
+        match self._bracket:
+            case "0,000-1,500%" | "1,500-2,000%":
+                factor = 1.05
+            case "2,001-2,500%":
+                factor = 1.1
+            case "2,501-3,000%":
+                factor = 1.15
+            case "3,001-3,500%" | "3,501-4,000%":
+                factor = 1.2
+            case "4,001-4,500%":
+                factor = 1.25
+            case "4,501-5,000%" | "5,001-5,500%":
+                factor = 1.3
+            case "5,501-6,000%":
+                factor = 1.35
+            case "6,001-6,500%" | "6,501-100,000%":
+                factor = 1.4
+        return (factor * self._student_debt / (self._month_interest / (1 - (1 + self._month_interest) ** -(self._period))))
+    
+    def _year_deductible_interest(self, annuity_gross_monthly_costs):
+        annuity_mortgage = self._max_mortgage
+        linear_mortgage = self._max_mortgage
+        annuity_year_deductible_interest = 0
+        linear_year_deductible_interest = 0
+
+        for _ in range(12):
+            linear_year_deductible_interest += linear_mortgage * self._month_interest
+            linear_mortgage -= linear_mortgage/self._period
+            
+            amount_interest = annuity_mortgage * self._month_interest
+            annuity_mortgage -= annuity_gross_monthly_costs - amount_interest
+            annuity_year_deductible_interest += amount_interest
+
+        return annuity_year_deductible_interest, linear_year_deductible_interest
         
+    def _notional_rental_value(self):
+        if self._woz <= 12500:
+            return 0
+        elif self._woz <= 25000:
+            return 0.0010 * self._woz
+        elif self._woz <= 50000:
+            return 0.0020 * self._woz
+        elif self._woz <= 75000:
+            return 0.0025 * self._woz
+        elif self._woz <= 1310000:
+            return 0.0035 * self._woz  
+    
+    def _annuity_costs(self):
+        return self._max_mortgage * (self._month_interest / (1 - (1 + self._month_interest) ** -self._period))
+    
+    def _linear_costs(self):
+        linear_mortgage_repayment = self._max_mortgage / self._period
+        linear_interest_payment = 0
+        linear_mortgage = self._max_mortgage
+
+        for _ in range(12):
+            linear_interest_payment += self._month_interest * linear_mortgage
+            linear_mortgage -= linear_mortgage_repayment
+
+        return linear_mortgage_repayment + linear_interest_payment / 12
+
+    def _new_max_mortgage(self):
+        return (self._annuity_costs() - self._costs) / (self._month_interest / (1 - (1 + self._month_interest) ** -self._period))
+    
     def find_max_mortgage(self):
-        return int(round((self._find_max_expense() / 100) * (self._income / 12) * self._find_annuity_factor(),0))
+        self._max_mortgage = int(round((self._find_max_expense() / 100) * (self._income / 12) * self._find_annuity_factor(),0))
+        self._max_mortgage += self._extra_mortgage_energy_label() - self._less_mortgage_student_debt()
+        self._max_mortgage = self._new_max_mortgage()
+        if self._max_mortgage > self._woz:
+            self._max_mortgage = self._woz
+        
+        return self._max_mortgage
+    
+    def monthly_costs(self):
+        if self._max_mortgage is None:
+            self.find_max_mortgage()
+
+        annuity_gross_monthly_costs = self._annuity_costs()
+        linear_gross_monthly_costs = self._linear_costs()
+
+        annuity_year_deductible_interest, linear_year_deductible_interest = self._year_deductible_interest(annuity_gross_monthly_costs)
+        notional_rental_value = self._notional_rental_value()
+
+        annuity_mortgage_interest_deduction = (annuity_year_deductible_interest - notional_rental_value) * self._perc_interest_deduction
+        linear_mortgage_interest_deduction = (linear_year_deductible_interest - notional_rental_value) * self._perc_interest_deduction
+        annuity_net_monthly_costs = annuity_gross_monthly_costs - annuity_mortgage_interest_deduction / 12
+        linear_net_monthly_costs = linear_gross_monthly_costs - linear_mortgage_interest_deduction / 12
+
+        return math.ceil(annuity_gross_monthly_costs), math.ceil(annuity_net_monthly_costs), math.ceil(linear_gross_monthly_costs), math.ceil(linear_net_monthly_costs)
     
 def main1():
-    income = 28000
+    income = 100000
     interest = 1.6
     period = 360
-    user = User(income, interest, period)
+    woz = 200000
+    user = User(income, interest, period, woz)
     print(user._find_max_expense())
     print(user._find_annuity_factor())
     print(user.find_max_mortgage())
 
 def main():
-    income = 100000
-    interest = 3.8
+    income = 50000
+    interest = 3.78
     period = 360
-    user = User(income, interest, period)
-    print(user._find_max_expense())
-    print(user._find_annuity_factor())
+    energy_label = "A+++"
+    woz = 133727
+    monthly_costs = 0
+    user = User(income, interest, period, energy_label, woz, monthly_costs)
+    print(user._find_max_expense(), user._find_annuity_factor())
     print(user.find_max_mortgage())
+    
+    print(user.monthly_costs())
 
 if __name__ == "__main__":
     main()
--- src/kb/kb_class.py
+++ src/kb/kb_class.py
@@ -1,5 +1,4 @@
 import json
-import re
 from .calculator import User
 
 
@@ -15,31 +14,38 @@ class KnowledgeBase:
         self._kb_item = None
 
     def _read_file(self, filename):
+        """Read the knowledge base file"""
         with open(filename, "r") as file:
             return json.load(file)
 
     def _update_facts(self, fact, value):
         """Update the facts dictionary."""
-        self._facts[fact] = value
+        if fact == "advice":
+            self._facts[fact].append(value)
+        else:
+            self._facts[fact] = value
 
     def _get_advice(self):
-        """Fetch the final advice if at the end step."""
+        """Get the advice given at the current step"""
         for rule in self._rules:
             if rule["description"] == self._current_step and "advice" in rule:
                 if "fill in" in rule:
-                    ls = []
-                    for item in rule["fill in"]:
-                        ls.append(self._facts[item])
-                    return rule["advice"].format(*ls)
+                    return rule["advice"].format(self._facts[rule["fill in"]])
                 return rule["advice"]
         return None
+    
+    def _conclusion(self):
+        """Return the conclusion containing all advice."""
+        conclusion = ""
+        for advice in self._facts["advice"]:
+            conclusion = conclusion + advice + "\n"
+        return conclusion.rstrip()
 
     def _find_step(self):
         """Find a knowledge base item by description."""
         for kb_item in self._kb:
             if kb_item["description"] == self._current_step:
                 self._kb_item = kb_item
-                self._current_question_index = 0
                 break
 
     def _condition(self, req):
@@ -53,20 +59,26 @@ class KnowledgeBase:
             else:
                 return None
         
-        if condition == "==":
-            return fact_value == value
-        elif condition == ">=":
-            return fact_value >= value
-        elif condition == "<=":
-            return fact_value <= value
-        elif condition == "!=":
-            return fact_value != value
+        match condition:
+            case "==":
+                return fact_value == value
+            case ">=":
+                return fact_value >= value
+            case "<=":
+                return fact_value <= value
+            case "!=":
+                return fact_value != value
         return False
 
     def _rule_deduction(self):
         """Apply rules to deduce the next step."""
         for rule in self._rules:
             if rule["description"] == self._current_step:
+                if "advice" in self._current_step:
+                    self._current_step = rule["next_step"]
+                    self._current_question_index = 0
+                    return
+
                 for req in rule["requirements"]:
                     if self._facts[req["name"]] == None:
                         return
@@ -76,54 +88,73 @@ class KnowledgeBase:
                         return
                     elif not condition:
                         self._current_step = rule["else"]
-                        self._find_step()
+                        self._current_question_index = 0
                         return
                     
                 self._current_step = rule["next_step"]
-                self._find_step()
+                self._current_question_index = 0
                 return
-                
-    def _check_requirements(self):
-        """Check if all requirements of the current step are satisfied."""
-        for rule in self._rules:
-            if rule["description"] == self._current_step:
-                for req in rule["requirements"]:
-                    if not self._condition(req["name"], req["condition"], req["value"]):
-                        self._current_step = rule["else"]
-                        self._find_step()
-                        return False
-                self._current_step = rule["next_step"]
-                self._find_step()
-                return True
     
-    def _calculate_mortgage(self):
-        user = User(self._facts["income"], round(self._facts["interest"],1), 360)
-        return user.find_max_mortgage()
-        
-    
+    def _interest_bank(self):
+        """Get the interest depending on the bank."""
+        if "ING" in self._current_step:
+            return 3.40
+        elif "Vista" in self._current_step:
+            return 3.34
+
+    def _update_mortgage_facts(self):
+        """Calculates the maximum mortgage."""
+        costs_per_month = sum(self._facts[item] for item in ("family loan", "other loan", "mobile phone on credit", "private lease car") if type(self._facts[item]) == int)
+        user = User(self._facts["income"], round(self._facts["interest"],1), 360, self._facts["energy label"], self._facts["property valuation"], costs_per_month, self._facts["student debt"])
+
+        maximum_mortgage = user.find_max_mortgage()
+        self._update_facts("maximum mortgage", maximum_mortgage)
+
+        annuity_gross, annuity_net, linear_gross, linear_net = user.monthly_costs()
+        self._update_facts("annuity gross monthly fees", annuity_gross)
+        self._update_facts("linear gross monthly fees", linear_gross)
+        self._update_facts("annuity net monthly fees", annuity_net)
+        self._update_facts("linear net monthly fees", linear_net)
+
     def question_or_advice(self):
+        """Infers which question to be asked or advice to be given to the user."""
         if self._current_question_index == 0:
             self._find_step()
 
-        if "advice" in self._current_step:
-            return "advice", self._get_advice(), None
+        if "bank" in self._current_step:
+            self._rule_deduction()
+
+        while "advice" in self._current_step:
+            if "bank" in self._current_step:
+                self._update_facts("interest", self._interest_bank())
+                if (self._facts["maximum mortgage"] is None
+                    and all(self._facts[key] is not None for key in ["income", "interest", "energy label", "property valuation", "student debt"])
+                    and self._facts["student debt"] != "yes"):
+                    self._update_mortgage_facts()
+
+            self._update_facts("advice", self._get_advice())
+
+            if "end advice" in self._current_step:
+                return "advice", self._conclusion(), None
+            else:
+                self._rule_deduction()
+                self._find_step()         
         
         if self._current_question_index < len(self._kb_item["requirement questions"]):
             self._kb_requirement = self._kb_item["requirement questions"][self._current_question_index]
+
             question = self._kb_requirement["question"]
+            if "fill in" in self._kb_requirement:
+                question = question.format(self._facts[self._kb_requirement["fill in"]])
             answer_type = self._kb_requirement["answer_type"]
 
             self._current_question_index += 1  # Move to the next question
             return "question", question, answer_type
         
     def answer(self, answer):
+        """Answer of the user is updated to the facts"""
         answer = (int(answer) if self._kb_requirement["answer_type"] == "integer" else answer)
-        self._update_facts(self._kb_requirement["name"], answer)
-
-        if self._facts["maximum mortgage"] is None and self._facts["income"] is not None and self._facts["interest"] is not None:
-            maximum_mortgage = self._calculate_mortgage()
-            self._update_facts("maximum mortgage", maximum_mortgage)
-
+        self._update_facts(self._kb_requirement["name"], answer)       
         self._rule_deduction()
 
 
