01697a07f4b5357d6e3caf92f78ade43af774ac9
BlokIan <i.d.blok@student.rug.nl>
2025-01-16 14:23:10 +0100

    Update readme

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from kivy.logger import Logger, LOG_LEVELS
 
 
 if __name__ == "__main__":
-    Logger.setLevel(LOG_LEVELS["debug"])
+    Logger.setLevel(LOG_LEVELS["info"])
     KnowledgeApp().run()
